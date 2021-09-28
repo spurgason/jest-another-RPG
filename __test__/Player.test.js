2929c69b8cede0499c35f1b7fca0a20d7b0f6a78
spurgason <shanepurgason.98@gmail.com>
2021-09-28 14:43:31 -0400

    Player constructor complete

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -56,5 +56,33 @@ test('subtracts from players health', () => {
     expect(player.health).toBe(oldHealth -5);
     player.reduceHealth(99999);
     expect(player.health).toBe(0);
-})
+});
+
+test("gets players attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('uses potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()]
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
+
 
